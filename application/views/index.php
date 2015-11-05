91d0817131a79def2ae9b32c394106c52bca7ec0
aoaelorlae <zcream.ae@windowslive.com>
2015-11-06 00:49:27 +0700

    git

---
+++ application/views/index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<!--
+ * A Design by GraphBerry
+ * Author: GraphBerry
+ * Author URL: http://graphberry.com
+ * License: http://graphberry.com/pages/license
+-->
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>ILMI: World of Knowledge</title>
+
+	<!-- Load fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+
+	<link rel="javascript" type="text/javascript" href="../../ilmiproject/asset/js/bootstrap.js">
+	<!-- Load css styles -->
+	<link rel="stylesheet" type="text/css" href="../../ilmiproject/asset/css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="../../ilmiproject/asset/css/font-awesome.css" />
+	<link rel="stylesheet" type="text/css" href="../../ilmiproject/asset/css/style.css" />
+
+</head>
+<body>
+	<div class="jumbotron home home-fullscreen" id="home">
+		<div class="mask"></div>
+		
+		<nav class="navbar navbar-inverse navbar-fixed-top" id="menubar" style="background-color: white;">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		          <span class="icon-bar"></span>
+		          <span class="icon-bar"></span>
+		          <span class="icon-bar"></span>                        
+		      </button>
+		      <a class="navbar-brand" href="#" style ="color:#3366FF;">ILMITUTOR.COM</a>
+		    </div>
+		    <div>
+		      <div class="collapse navbar-collapse col-lg-10" id="myNavbar">
+		        <ul class="nav navbar-nav">
+		          <li><a href="#home" id="menu" >Home</a></li>
+		          <li><a href="#about" >About Us</a></li>
+		        </ul>
+				
+				<style type="text/css">
+					ul li:hover {background: #3366FF;}
+					ul li:hover ul {display: block;}
+				</style>	        
+
+		      </div>
+		        <ul class="nav navbar-right top-nav" >
+		            
+		        </ul>
+		    </div>
+		  </div>
+		</nav>
+
+		<div class="container">
+			<div class="header-info">
+				<h1>ILMI</font>
+				<p><i>ILMI</i>   in Swedish is mean 'Knowledge'<br>
+				<font size=-1>Kingdom of Learning and prepare for yourself to study or increase your score.<br>
+				You can find fit teacher(s) in many lessons and more interested things with us. Come and join!!</font></p>
+				<a href="#" class="btn btn-primary">JOIN US</a>
+			</div>
+		</div>
+	</div>
+	<!-- Services section start -->
+	<section id="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="service-item">
+						<div class="icon"><i class="fa fa-diamond" ></i></div>
+						<h3 style="color:#3366ff">We Are Less Difficult</h3>
+						<p>Design for many ages of anonymous and have discription in every step to use ILMITUTOR
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="service-item">
+						<div class="icon"><i class="fa fa-diamond"></i></div>
+						<h3 style="color:#3366ff">We Are Beautiful</h3>
+						<p>Our team produced and easy and beauty website สวยยยยยยยยยยยย
+						</p>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="service-item">
+						<div class="icon"><i class="fa fa-diamond"></i></div>
+						<h3 style="color:#3366ff">We Know You Want Us</h3>
+						<p>สื่อกลางเครือข่ายการติดต่อระหว่่างติวเตอร์และนักเรียนที่สนใจ คุณสามารถค้นหาวิชาที่สนใจทั้งเรียนหรือสอนได้จากพวกเรา ทางเราคัดสรรบุคคลสมาชิกที่มีตัวตนจริง มีการตรวจสอบก่อนยืนยันการใช้งาน
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	 
+	<!-- Portfolio section end -->
+	<!-- Contact section start -->
+	<section id="contact">
+		<div class="container">
+			<header>
+				<h2 style ="color:#3366ff;">Get in Touch</h2>
+				<p>เราอยู่ไม่ไกล โปรดอย่ามองข้าม มามะ มาจอยกัน
+				</p>
+			</header>
+			<div class="map-wrapper">
+				<div class="map-canvas" id="map-canvas">Loading map...</div>
+				<div class="marker">
+					<span>We Are Here</span>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8">
+					<form class="row">
+						<div class="form-group col-md-6">
+							<input name="name" type="text" placeholder="Your Name" class="form-control" />
+						</div>
+						<div class="form-group col-md-6">
+							<input name="mail" type="email" placeholder="Your Email" class="form-control" />
+						</div>
+						<div class="form-group col-md-12">
+							<input name="subject" type="text" placeholder="Type Message Here..." class="form-control" />
+						</div>
+						<div class="form-group col-md-12">
+							<textarea name="message" class="form-control" rows="10" placeholder="Message"></textarea>
+						</div>
+						<div class="form-group col-md-12">
+							<button class="btn btn-lg btn-primary">Send</button>
+						</div>
+					</form>
+				</div>
+				<div class="col-md-3 col-md-offset-1">
+					<address>
+						<span>Our Address</span>
+						<p>
+							FIET CB3, KMUTT<br>
+							Prachautid Rd. Bangmod Thungkru<br>
+							Bangkok :)
+						</p>
+					</address>
+					<address>
+						<span>Email </span>
+						<p>ilmitutor@ilmi.com</p>
+					</address>
+					<address>
+						<span>Phone</span>
+						<p>1150</p>
+					</address>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Contact section end -->
+	<!-- Footer start -->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					
+				</div>
+				<div class="col-md-4">
+					<ul class="social-icons">
+						<li><a href=""><i class="fa fa-facebook"></i></a></li>
+						<li><a href=""><i class="fa fa-twitter"></i></a></li>
+						<li><a href=""><i class="fa fa-dribbble"></i></a></li>
+						<li><a href=""><i class="fa fa-behance"></i></a></li>
+						<li><a href=""><i class="fa fa-pinterest"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- Footer end  -->
+
+	<!-- Load jQuery -->
+	<script type="text/javascript" src="../../ilmiproject/asset/js/jquery-1.11.2.min.js"></script>
+
+	<!-- Load Booststrap -->
+	<script type="text/javascript" src="../../ilmiproject/asset/js/bootstrap.js"></script>
+
+	<script type="text/javascript" src="../../ilmiproject/asset/js/smooth-scroll.js"></script>
+
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+	<!-- Load custom js for theme -->
+	<script type="text/javascript" src="../../ilmiproject/asset/js/app.js"></script>
+</body>
+</html>
