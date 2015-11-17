65ba2e3071e3e38ae6efd6ac9468e75a3d85b4eb
virachart <firstza22@gmail.com>
2015-11-18 01:57:32 +0700

    homestuden

--- application/views/stuhome.php
+++ application/views/stuhome.php
@@ -87,7 +87,7 @@
                     <div class="col-lg-12">
                         <div class="col-lg-2"></div>
                         <div class="col-lg-8">
-                            <img src='<?=base_url()?>images/icon/step.png'>
+                            <img src='<?=base_url()?>images/icon/stepstu.png'>
                         </div>
                         <div class="col-lg-2"></div>
 
@@ -102,19 +102,14 @@
                             <?php echo anchor("tutorprofile", "<img src='".base_url()."/images/icon/profile.png'>");?>
                         </div>
                         <div class="col-lg-3">
-                            <?php echo anchor("tutorlog", "<img src='".base_url()."/images/icon/statistics.png'>");?>
+                            <?php echo anchor("tutorlog", "<img src='".base_url()."/images/icon/my tutor.png'>");?>
                         </div>
                         <div class="col-lg-3">
-                            <?php echo anchor("tutorpayment", "<img src='".base_url()."/images/icon/money.png'>");?>
+                            <?php echo anchor("tutorpayment", "<img src='".base_url()."/images/icon/request.png'>");?>
                         </div>
 
                     </div>
-                    <div class="col-lg-12">
-                        <hr style="border-width: 5px;border-color: #3366ff;">
-                    </div>
-                    <h2 style="color:#3366ff; margin-left: 165px;font-weight: bold;">TOP RARE</h2>
-                    <p><h4 style="color:#3366ff; margin-left: 165px;font-weight: bold;">วิชายอดนิยมที่เด็กๆ ต้องการติวเตอร์</p></h4>
-
+                    
              <center>
                 <div class="row">
                     <div class="col-lg-12">
